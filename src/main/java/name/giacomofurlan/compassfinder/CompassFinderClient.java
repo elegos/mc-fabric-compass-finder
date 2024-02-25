7a7c62f92da730b9efabf78731aaa212cbbdc371
Giacomo "Mr. Wolf" Furlan <git@giacomofurlan.name>
2024-02-25 13:56:04 +0100

    0.5: update compass pointing when breaking the pointed block

--- src/main/java/name/giacomofurlan/compassfinder/CompassFinderClient.java
+++ src/main/java/name/giacomofurlan/compassfinder/CompassFinderClient.java
@@ -1,9 +1,38 @@
 package name.giacomofurlan.compassfinder;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.client.player.ClientPlayerBlockBreakEvents;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.CompassItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.util.math.BlockPos;
 
 public class CompassFinderClient implements ClientModInitializer {
 
     @Override
-    public void onInitializeClient() {}
+    public void onInitializeClient() {
+        ClientPlayerBlockBreakEvents.AFTER.register((ClientWorld world, ClientPlayerEntity player, BlockPos pos, BlockState state) -> {
+            PlayerInventory inventory = player.getInventory();
+
+            for (int slot = 0; slot < inventory.size(); slot++) {
+                ItemStack stack = inventory.getStack(slot);
+                NbtCompound nbt = stack.getNbt();
+                Boolean moddedCompass = nbt != null && nbt.contains(CompassFinder.MODDED_COMPASS_ORE_KEY);
+                BlockPos lodestoneBlockPos = nbt != null && nbt.contains(CompassItem.LODESTONE_DIMENSION_KEY)
+                    ? NbtHelper.toBlockPos(nbt.getCompound(CompassItem.LODESTONE_POS_KEY))
+                    : null;
+                
+                if (moddedCompass && lodestoneBlockPos.equals(pos)) {
+                    InventoryHelper.updateCompasses();
+
+                    break;
+                }
+            }
+        });
+    }
 }
