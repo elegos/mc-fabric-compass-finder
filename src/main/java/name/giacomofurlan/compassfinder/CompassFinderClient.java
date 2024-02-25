7d70667dd15a192ae45cd03988cf66cefd273445
Giacomo "Mr. Wolf" Furlan <git@giacomofurlan.name>
2024-02-25 18:18:03 +0100

    WIP

--- src/main/java/name/giacomofurlan/compassfinder/CompassFinderClient.java
+++ src/main/java/name/giacomofurlan/compassfinder/CompassFinderClient.java
@@ -1,8 +1,10 @@
 package name.giacomofurlan.compassfinder;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.event.client.player.ClientPlayerBlockBreakEvents;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerInventory;
@@ -17,22 +19,34 @@ public class CompassFinderClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientPlayerBlockBreakEvents.AFTER.register((ClientWorld world, ClientPlayerEntity player, BlockPos pos, BlockState state) -> {
-            PlayerInventory inventory = player.getInventory();
-
-            for (int slot = 0; slot < inventory.size(); slot++) {
-                ItemStack stack = inventory.getStack(slot);
-                NbtCompound nbt = stack.getNbt();
-                Boolean moddedCompass = nbt != null && nbt.contains(CompassFinder.MODDED_COMPASS_ORE_KEY);
-                BlockPos lodestoneBlockPos = nbt != null && nbt.contains(CompassItem.LODESTONE_DIMENSION_KEY)
-                    ? NbtHelper.toBlockPos(nbt.getCompound(CompassItem.LODESTONE_POS_KEY))
-                    : null;
-                
-                if (moddedCompass && lodestoneBlockPos.equals(pos)) {
-                    InventoryHelper.updateCompasses();
-
-                    break;
-                }
-            }
+            this.onClientPlayerBlockBreakEventsCallback(world, player, pos, state);
         });
+
+        HudRenderCallback.EVENT.register((DrawContext drawContext, float tickDelta) -> {
+            onHudRenderCallback(null, 0);
+        });
+    }
+
+    public void onClientPlayerBlockBreakEventsCallback(ClientWorld world, ClientPlayerEntity player, BlockPos pos, BlockState state) {
+        PlayerInventory inventory = player.getInventory();
+
+        for (int slot = 0; slot < inventory.size(); slot++) {
+            ItemStack stack = inventory.getStack(slot);
+            NbtCompound nbt = stack.getNbt();
+            Boolean moddedCompass = nbt != null && nbt.contains(CompassFinder.MODDED_COMPASS_ORE_KEY);
+            BlockPos lodestoneBlockPos = nbt != null && nbt.contains(CompassItem.LODESTONE_DIMENSION_KEY)
+                ? NbtHelper.toBlockPos(nbt.getCompound(CompassItem.LODESTONE_POS_KEY))
+                : null;
+            
+            if (moddedCompass && lodestoneBlockPos.equals(pos)) {
+                InventoryHelper.updateCompasses();
+
+                break;
+            }
+        }
+    }
+
+    public static void onHudRenderCallback(DrawContext drawContext, float tickDelta) {
+        InventoryHelper.updateHotbarDistances(drawContext, tickDelta);
     }
 }
--- src/main/java/name/giacomofurlan/compassfinder/InventoryHelper.java
+++ src/main/java/name/giacomofurlan/compassfinder/InventoryHelper.java
@@ -1,16 +1,57 @@
 package name.giacomofurlan.compassfinder;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 import name.giacomofurlan.compassfinder.services.CompassManager;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.CompassItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class InventoryHelper {
+    public static void updateHotbarDistances(DrawContext drawContext, float tickDelta) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayerEntity player = client.player;
+        Optional<NbtElement> instanceNbt = World.CODEC.encodeStart(NbtOps.INSTANCE, player.getWorld().getRegistryKey())
+            .resultOrPartial(CompassFinder.LOGGER::error);
+
+        if (drawContext == null || player == null || instanceNbt.isEmpty()) {
+            return;
+        }
+
+        PlayerInventory inventory = player.getInventory();
+        for (int slot = 0; slot < PlayerInventory.getHotbarSize(); slot++) {
+            ItemStack stack = inventory.getStack(slot);
+            NbtCompound nbt = stack.getNbt();
+
+            if (
+                nbt == null
+                || !nbt.contains(CompassFinder.MODDED_COMPASS_ORE_KEY)
+                || !nbt.contains(CompassItem.LODESTONE_POS_KEY)
+                || !nbt.getString(CompassItem.LODESTONE_DIMENSION_KEY).equals(instanceNbt.get().asString())
+            ) {
+                continue;
+            }
+
+            Integer distance = (int) NbtHelper.toBlockPos(nbt.getCompound(CompassItem.LODESTONE_POS_KEY)).getSquaredDistance(player.getPos());
+
+            drawCustomTextOnHotbar(drawContext, stack);
+            stack.setCount(distance);
+        }
+    }
+
     public static void updateCompasses() {
         MinecraftClient client = MinecraftClient.getInstance();
         ClientPlayerEntity player = client.player;
@@ -35,5 +76,18 @@ public class InventoryHelper {
                 CompassManager.updateCompassPos(player, option, pos, stack);
             }
         }
+    }
+
+    private static void drawCustomTextOnHotbar(DrawContext drawContext, ItemStack itemStack) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        TextRenderer textRenderer = client.textRenderer;
+
+        int screenWidth = client.getWindow().getScaledWidth();
+        int screenHeight = client.getWindow().getScaledHeight();
+
+        int x = screenWidth / 2 - 91;
+        int y = screenHeight - 22;
+
+        drawContext.drawText(textRenderer, Text.of("99"), x, y, y, false);
     }
 }
